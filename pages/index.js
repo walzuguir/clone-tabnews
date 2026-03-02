56dce89682d64e38e265a353328711db48332550
Wilson Alzuguir <96277058+walzuguir@users.noreply.github.com>
2026-03-02 23:15:09 +0000

    parenteses no JSX

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h3>é um conceito central do budismo que afirma que todos os fenômenos e sofrimentos surgem interconectados, resultando de causas e condições, e não de forma independente</h3>
-}
+    return( <h3>é um conceito central do budismo que afirma que todos os fenômenos e sofrimentos surgem interconectados, resultando de causas e condições, e não de forma independente</h3>
+);
+    }
 
 export default Home;

cc44b23d7c88ef9d54b31a260195c04bd0153b63
Wilson Alzuguir <96277058+walzuguir@users.noreply.github.com>
2026-03-02 23:10:38 +0000

    Mudança para frase budista

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Primeira mensagem do site!!</h1>
+    return <h3>é um conceito central do budismo que afirma que todos os fenômenos e sofrimentos surgem interconectados, resultando de causas e condições, e não de forma independente</h3>
 }
 
 export default Home;

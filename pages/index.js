d6eeaa81ee7e2722c5d54b25726a300c2a16a905
Wilson Alzuguir <96277058+walzuguir@users.noreply.github.com>
2026-03-02 23:24:51 +0000

    correcao do projeto para funcionamento pleno

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-function Home(){
-    return( <h3>é um conceito central do budismo que afirma que todos os fenômenos e sofrimentos surgem interconectados, resultando de causas e condições, e não de forma independente</h3>
-);
-    }
+function Home() {
+  return (
+    <h3>
+      Originação Dependente é um conceito central do budismo que afirma que todos os fenômenos e
+      sofrimentos surgem interconectados, resultando de causas e condições, e
+      não de forma independente
+    </h3>
+  );
+}
 
 export default Home;
